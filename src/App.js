7d6342ed3a17d9664faafeb4311f896a7efed28c
Waqardev <Waqardev@users.noreply.github.com>
2019-12-30 18:50:14 +0500

    reverse change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with chaudhry mazhar
+          Learn React
         </a>
       </header>
     </div>
