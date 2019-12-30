8f6a96e0d9712a6e7190866b03808383a081d6bb
Waqardev <Waqardev@users.noreply.github.com>
2019-12-31 00:12:58 +0500

    App completed Without Style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React,{ Component } from 'react';
+import CardList from './CardList';
+import { robots } from './robots.js';
+import SearchBox from './SearchBox'; 
 
 
-function App() {
+class App extends Component {
+
+   constructor(){
+   	        super();
+
+          	this.state={
+					robots:robots,
+					searchfield:''
+}
+
+          }
+
+onSearchChange=(event)=>{
+	this.setState({searchfield:event.target.value})
+		
+}
+
+        render(){
+
+        const filteredRobots=this.state.robots.filter(robot=>{
+		return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+	})
+
   return (
-        <h1>Home</h1>
+  	     <div className='tc'>
+	  	     <h1>RoboFriends</h1>
+             <SearchBox searchChange={this.onSearchChange}/>
+	  	     <CardList robots={filteredRobots} />
+  	     </div>
   	);
+}
 }
 
 export default App;
