a34e44d3fed3062a95ff0c2030b3fe417ce2955f
Waqardev <Waqardev@users.noreply.github.com>
2019-12-31 11:48:39 +0500

    Style App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{ Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox'; 
 import Scroll from './Scroll';
+import './App.css';
 
 
 class App extends Component {
@@ -39,7 +40,7 @@ componentDidMount(){
 
   return (
   	     <div className='tc'>
-	  	     <h1>RoboFriends</h1>
+	  	     <h1 className='f1'>RoboFriends</h1>
              <SearchBox searchChange={this.onSearchChange}/>
              <Scroll>
 	  	        <CardList robots={filteredRobots} />
